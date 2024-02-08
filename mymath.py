05f916746fe360de9840fce1a8fa5f33065155d4
seoyoung <psy20116@gmail.com>
2024-02-08 11:08:28 +0900

    v0.5 performance check

--- mymath.py
+++ mymath.py
@@ -1,8 +1,40 @@
-import mymath
+import time
 
-if __name__ == "__main__":
-    n = int(input("Input n : "))
-    r = int(input("Input r : "))
-    print(f"{n}C{r} = {mymath.nCr(n, r)}")
-    # f = int(input())
-    # print(mymath.factorial(f))
+
+# def factorial(number) -> int:
+#     '''
+#     factorial by repetition
+#     :param number: number (int)
+#     :return: factorial result (int)
+#     '''
+#     result = 1
+#     for i in range(1, number+1):
+#         result = result * i
+#     return result
+
+
+def factorial(number) -> int:
+    '''
+    factorial by recursion
+    :param number: number (int)
+    :return: factorial result (int)
+    '''
+    if number == 1:
+        return 1
+    else:
+        return number * factorial(number - 1)
+
+
+def nCr(n, r) -> int:
+    '''
+    combination function
+    :param n:
+    :param r:
+    :return:
+    '''
+    start = time.time()
+    numerator = factorial(n)
+    denominator = factorial(n-r) * factorial(r)
+    end = time.time()
+    print(f"소요시간 : {end - start}")
+    return int(numerator / denominator)
